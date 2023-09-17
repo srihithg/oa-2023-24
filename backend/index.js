e9703bc744fcb5d89a3047e49e053d719f7838f2
Srihith Garlapati <srihith.garlapati@gmail.com>
2023-09-17 11:56:37 -0500

    Basic styling added

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5500"
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5500",
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
-    }).status(200)
-})
+  res
+    .send({
+      x: [1, 2, 3],
+      y: [4, 5, 6],
+    })
+    .status(200);
+});
 
-
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`server listening on port ${port} 🚀`))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server listening on port ${port} 🚀`));
